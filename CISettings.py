4a434e0161a8ec505137d2ec8eb9a218095a2526
Bret Barkelew <brbarkel@microsoft.com>
2019-02-01 13:44:21 -0800

    REBASE: Start branch documentation and pivot to validation CI.

--- CISettings.py
+++ CISettings.py
@@ -55,12 +55,12 @@ class Settings(CiBuildSettingsManager, CiSetupSettingsManager, UpdateSettingsMan
             {
                 "Path": "Common/MU_TIANO",
                 "Url": "https://github.com/Microsoft/mu_tiano_plus.git",
-                "Branch": "dev/201905"
+                "Branch": "dev/201908"
             },
             {
                 "Path": "MU_BASECORE",
                 "Url": "https://github.com/Microsoft/mu_basecore.git",
-                "Branch": "dev/201905"
+                "Branch": "dev/201908"
             }
         ]
 
