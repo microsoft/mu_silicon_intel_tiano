1b98c2794537f9286f978a07ec57ff7186cdec5b
Aaron Antone <aanton@microsoft.com>
2019-07-09 04:27:44 +0000

    Merged PR 1139: Delay IOMMU Install until DMAR is available
    Delay IOMMU Install until DMAR is available

--- IntelSiliconPkg/Feature/VTd/IntelVTdDxe/DmaProtection.c
+++ IntelSiliconPkg/Feature/VTd/IntelVTdDxe/DmaProtection.c
@@ -556,6 +556,7 @@ AcpiNotificationFunc (
   )
 {
   EFI_STATUS          Status;
+  EFI_HANDLE          Handle;  //MU_CHANGE
 
   Status = GetDmarAcpiTable ();
   if (EFI_ERROR (Status)) {
@@ -565,6 +566,18 @@ AcpiNotificationFunc (
     return;
   }
   SetupVtd ();
+
+ // MU_CHANGE [BEGIN] - Delay IOMMU protocol install until DMAR table has been initialized
+  Handle = NULL;
+  Status = gBS->InstallMultipleProtocolInterfaces (
+                  &Handle,
+                  &gEdkiiIoMmuProtocolGuid, &mIntelVTd,
+                  NULL
+                  );
+  ASSERT_EFI_ERROR (Status);
+  // MU_CHANGE [END]
+
+
   gBS->CloseEvent (Event);
 }
 
