e8517a6384f7a5aa7eb2ddf3f308fa9fd0151954
Bret Barkelew <bret.barkelew@microsoft.com>
2020-10-08 16:37:37 -0700

    Sync pipelines and requirements with Basecore

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -131,7 +131,7 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
             is_linux = GetHostInfo().os.upper() == "LINUX"
 
             if self.UseBuiltInBaseTools is None:
-                is_linux = GetHostInfo().os.upper() == "LINUX"
+                # MU_CHANGE - redundant is_linux = GetHostInfo().os.upper() == "LINUX"
                 # try and import the pip module for basetools
                 try:
                     import edk2basetools
@@ -184,13 +184,13 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
         return [
             {
                 "Path": "Common/MU_TIANO",
-                "Url": "https://github.com/Microsoft/mu_tiano_plus.git",
-                "Branch": "release/202005"
+                "Url": "https://github.com/microsoft/mu_tiano_plus.git",
+                "Branch": "release/202108"
             },
             {
                 "Path": "MU_BASECORE",
-                "Url": "https://github.com/Microsoft/mu_basecore.git",
-                "Branch": "release/202005"
+                "Url": "https://github.com/microsoft/mu_basecore.git",
+                "Branch": "release/202108"
             }
         ]
         # MU_CHANGE END
