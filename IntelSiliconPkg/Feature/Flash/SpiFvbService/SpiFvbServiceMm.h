d9c34c227e7a876f419db3630ef8871561d8201a
Aaron Pop <aaronpop@microsoft.com>
2024-06-18 10:11:42 -0700

    IntelSiliconPkg: Bring in IntelSiliconPkg from Edk2-Platforms.
    Bring in IntelSiliconPkg.
    Apply CI fixes to IntelSiliconPkg, uncrustify.
    Address build errors for IA32 builds.
    
    Co-Authored-By: Michael Kubacki <michael.kubacki@microsoft.com>
    Co-Authored-By: kenlautner <85201046+kenlautner@users.noreply.github.com>

---
+++ IntelSiliconPkg/Feature/Flash/SpiFvbService/SpiFvbServiceMm.h
@@ -0,0 +1,22 @@
+/** @file
+  Definitions common to MM implementation in this driver.
+
+  Copyright (c) Microsoft Corporation.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef SPI_FVB_SERVICE_MM_H_
+#define SPI_FVB_SERVICE_MM_H_
+
+/**
+  The function does the necessary initialization work for
+  the Firmware Volume Block Driver.
+
+**/
+VOID
+FvbInitialize (
+  VOID
+  );
+
+#endif
